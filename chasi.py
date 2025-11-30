c09f9a320e4b0276493d93fa051d432d6201c7fe
ddadka <dz10102010@gmail.com>
2025-11-30 22:41:40 +0300

    Update chasi.py

--- chasi.py
+++ chasi.py
@@ -1,6 +1,47 @@
-hours, minutes = map(int, input().split())
+'''
+алгоритм
+
+вывод приветственного сообщения с просьбой ввести время
+ввод времени
 
 
+функция на проверку правильного ввода часов и минут:
+ 1.проверка на то что и часы и минуты введены неправильно
+ 2.проверка на то что часы введены не правильно
+ 3.проверка на то что минуты введены не правлиьно
+ 
+ 
+функция на проверку минут:
+ 1.минуты равны нулю
+ 2.количество минут заканчивается на 1
+ 3.количество минут заканчивается на цифры от 5 до 9 и 0 или минуты от 10 до 20
+ 4.количество минут заканчивается на цифры от 2 до 4
+аналогичная фйнкция для проверки часов
+
+
+функция на проверку времени суток:
+ 1.от 0 до 5 часов ночь
+ 2.от 6 до 11 часов утро
+ 3.от 12 до 17 часов день
+ 4.от 18 до 23 часов вечер
+ 
+ 
+проверка на вверную длину ввода:
+ вывод верной ошибки
+проверка на положительность ввода:
+ вывод верной ошибки
+использование функции на проверку правильного вводы часов и минут:
+ вывод верной ошибки 
+проверка на полдень и полночь:
+ вывод ответа в правильном формате 
+проверка на то что кол-во минут равно 0:
+ вывод ответа в правильном формате 
+'''
+print('введите часы и минуты:')
+vrem = list(map(str, input().split()))
+hours=vrem[0]
+minutes=vrem[1]
+dlin_vvod=len(vrem)
 def prov(hours, minutes):
     if (hours < 0 or hours > 23) and (minutes < 0 or minutes > 59):
         return print('Введены недопустимые данные: часы и минуты должны быть в пределах значений')
@@ -43,14 +84,16 @@ def vrem_sut(hours):
         return 'дня'
     if 18 <= hours <= 23:
         return 'вечера'
-
-
-if prov(hours, minutes) == '1':
+if dlin_vvod!=2:
+    print('Введены неверные данные: значения должны быть введины как XX XX')    
+elif hours[0]=='-' or minutes[0]=='-':
+    print('Введены неверные данные:все значения должны быть положительными')
+elif prov(int(hours),int(minutes)) == '1':
     if str(hours) + str(minutes) == '00':
         print('полночь')
     elif str(hours) + str(minutes) == '120':
         print('полдень')
     elif prov_min(int(minutes)) != ' ровно':
-        print(str(hours) + prov_chas(int(hours)) + str(minutes) + prov_min(int(minutes)) + vrem_sut(hours))
+        print(str(int(hours)) + prov_chas(int(hours)) + str(int(minutes)) + prov_min(int(minutes)) + vrem_sut(int(hours)))
     else:
-        print(str(hours) + prov_chas(int(hours)) + vrem_sut(hours) + prov_min(int(minutes)))
+        print(str(int(hours)) + prov_chas(int(hours)) + vrem_sut(int(hours)) + prov_min(int(minutes)))
