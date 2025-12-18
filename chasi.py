4b72df1ab15d355e175466e57f02288f8a866c8a
ddadka <dz10102010@gmail.com>
2025-12-18 21:03:06 +0300

    Update chasi.py

--- chasi.py
+++ chasi.py
@@ -88,7 +88,7 @@ def main():
         minutes=vrem[1]
         hours_znach=int(hours)
         minutes_znach=int(minutes)    
-        if prov(hours_znach,hours_znach) == '1':
+        if prov(hours_znach,minutes_znach) == '1':
             if hours + minutes == '0000':
                 print('полночь')
             elif hours + minutes == '1200':
@@ -99,3 +99,4 @@ def main():
                 print(hours + prov_chas(hours_znach) + vrem_sut(hours_znach) + prov_min(minutes_znach))
 if __name__ == "__main__":
     main()
+
